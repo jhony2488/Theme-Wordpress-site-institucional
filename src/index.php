899402e78a3ae241005ea3505b8b71d91bba120b
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-01-05 20:21:10 -0300

    feitura do menu mobile

--- src/index.php
+++ src/index.php
@@ -25,7 +25,20 @@
                 <img src="./assets/images/home/pngs/logo.png" alt="Logo da empresa">
             </div>
             <nav class="nav">
-                <ul>
+                <input type="checkbox" id="control-nav" />
+                <label for="control-nav" class="control-nav">
+                    <div class="control-nav__line">
+                        <div class="control-nav__line-wrapper">
+                            <div class='line-hamburguer-button'></div>
+                            <div class='line-hamburguer-button-half'></div>
+                            <div class='line-hamburguer-button'></div>
+                        </div>
+                    </div>
+                </label>
+                <label for="control-nav" class="control-nav-close">
+                    <i class="fas fa-times"></i>
+                </label>
+                <ul class="ul">
                     <li>
                         <a href="index.php">Home</a>
                     </li>
