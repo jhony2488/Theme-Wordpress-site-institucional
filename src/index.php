1ed9d92b31a05ea24f1c902224ea456fa4363ea5
Jhonata Vinicius <jhon.araujo2488@gmail.com>
2021-01-05 19:15:09 -0300

    feitura do banner e nav e header

---
+++ src/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Setup Theme Wordpress</title>
+    <!-- Esconde as mudanças visuais da biblioteca de acessibilidade. -->
+    <!--[if lt IE 9]>
+            <script src="./assets/scripts/js/jQuery.js"></script>
+            <script async defer src="./assets/scripts/js/allPolify.js"></script>
+        <![endif]-->
+    <!--[if (gte IE 6)&(lte IE 8)]>
+            <script src="./assets/scripts/js/jQuery.js"></script>
+            <script async defer src="./assets/scripts/js/allPolify.js"></script>
+        <![endif]-->
+    <link rel="stylesheet" type="text/css" href="./assets/css/main.css" media="print" onload="this.media='all'" />
+    <link href="./assets/fontawesome/css/all.css" rel="stylesheet" type="text/css" media="print" onload="this.media='all'" />
+</head>
+
+<body>
+    <header class="header">
+        <div class="header-wrapper">
+            <div class="header-logo">
+                <img src="./assets/images/home/pngs/logo.png" alt="Logo da empresa">
+            </div>
+            <nav class="nav">
+                <ul>
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.php">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <main class="main">
+        <section class="banner">
+            <div class="banner-wrapper">
+                <div class="banner__item">
+                    <h2>Melhore a comunicação com o seu cliente e time</h2>
+                    <p>Consultoria especializada em startups,empresas, principalmente pessoas </p>
+                    <div class="banner__item-button">
+                        <a href="">Ver demonstração</a>
+                    </div>
+                </div>
+                <div class="banner__item">
+                    <img src="./assets/images/home/pngs/ilustracao.png" alt="">
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="footer">
+
+    </footer>
+</body>
+<script src="./assets/scripts/js/jQuery.js"></script>
+<!-- Faz com que seja possível armazenar todos os event listener da página. -->
+<script type="text/javascript" src="./assets/acessibilidade/js/eventlistener.js"></script>
+<!-- Arquivos de idiomas -->
+<script type="text/javascript" src="./assets/acessibilidade/_locales/en_US/js/configurations.js"></script>
+<!-- Dependências -->
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/CommonFunctions.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/Configure.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/html/vanilla/VanillaHTMLDOMParser.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/html/vanilla/VanillaHTMLDOMElement.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/html/vanilla/VanillaHTMLDOMTextNode.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/css/jscssp/JSCSSPParser.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/css/jscssp/JSCSSPRule.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/util/css/jscssp/JSCSSPDeclaration.js"></script>
+<!-- Classes de soluções -->
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/implementation/AccessibleCSSImplementation.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/implementation/AccessibleEventImplementation.js"></script>
+<script type="text/javascript" src="./assets/acessibilidade/js/hatemile/implementation/AccessibleFormImplementation.js"></script>
+<script async defer src="./assets/fontawesome/js/all.js"></script>
+<script async defer src="./assets/scripts/js/acessibilidade.js"></script>
+
+</html>
